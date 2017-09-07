efad6a4e9c3733445f42741d98d04ed5216a3761
jacobh2299 <31513783+jacobh2299@users.noreply.github.com>
2017-09-07 08:11:07 -0600

    Started the runner file and added the controller and runner classes including the main method

---
+++ src/sample/controller/SampleRunner.java
@@ -0,0 +1,12 @@
+package sample.controller;
+
+public class SampleRunner {
+
+	public static void main(String[] args) 
+	{
+		SampleController myApp = new SampleController();
+		myApp.start();
+		
+	}
+
+}
